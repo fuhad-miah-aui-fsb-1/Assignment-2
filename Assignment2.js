760c2a7af294e96cbf6dc6c3e379ff1084db31b3
fuhad-miah-aui-fsb-1 <fuhadmiah5615@gmail.com>
2020-06-04 20:17:53 -0400

    modified assignment2.html

---
+++ Assignment2.js
@@ -0,0 +1,23 @@
+// 1.Find the square of a given number
+// ans. To find the square of a given number just multiply given number by itself.
+
+
+// 2.Look at the time of the day and tell whether it's time for breakfast, lunch or dinner.
+// ans. First i need three  time set 
+// second assign them with breakfast, lunch or dinner such as: if time is between 9 to 10 am it's breakfast. 
+// else time is between 1 to 2 pm lunch. else time is between 8 to 9 pm it's dinner.
+// Ask the user to input time and it will tell weather it's breakfast, lunch or dinner.
+
+
+// 3. Given a number, find whether the number is odd or even
+// ans. Fist ask the user input a number.
+// second divide the number by 2 
+// third if the has no reminder number is even or else it's odd
+
+
+// 4. Find out whether a given number or a string is palindrome(something that looks the same reading straight or in reverse)
+// ans. 
+
+
+
+// 5. Find the 10th number of a fibonacci series
